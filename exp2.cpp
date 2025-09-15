2c052dba89037d27a36ee9b1e90c7b1c78c5e397
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-09-15 15:18:52 +0530

    Update exp2.cpp

--- exp2.cpp
+++ exp2.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
 
-// Recursive function to find factorial
 int factorial(int n) {
-    if (n == 0 || n == 1)  // base case
+    if (n == 0 || n == 1) 
         return 1;
     else
-        return n * factorial(n - 1); // recursive case
+        return n * factorial(n - 1); 
 }
 
 int main() {
