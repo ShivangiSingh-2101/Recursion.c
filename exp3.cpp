44954ee6bc07ae97893eba8f812737f6abb360f8
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-09-15 15:13:01 +0530

    Create exp3.cpp

---
+++ exp3.cpp
@@ -0,0 +1,29 @@
+//Shivangi Singh
+//24070123148
+#include <iostream>
+using namespace std;
+
+// Recursive function to print string in reverse
+void reverseString(string str, int index) {
+    if (index < 0)
+        return; // base case
+    cout << str[index];
+    reverseString(str, index - 1); // recursive call
+}
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input); // allows spaces in input
+
+    cout << "Reversed string: ";
+    reverseString(input, input.length() - 1);
+
+    cout << endl;
+    return 0;
+}
+/*
+Output
+Enter a string: Shivangi
+Reversed string: ignavihS
+*/
